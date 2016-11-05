af5a33e4904db0433a402dbad5b1db16ca6634c1
Ximalis <ximalis@yandex.ru>
2016-11-05 18:50:30 +0200

    Update code

--- palindromes.php
+++ palindromes.php
@@ -1,11 +1,35 @@
 <?php
-$str = 'qweewq';
-$str2 = '';
-for ($i=strlen($str)-1; $i>=0; $i--){
-    $str2 .= $str[$i];
+$str = 'qweewqw';
+$str = preg_replace("|[^\d\w ]+|i","",$str);
+$str = preg_replace("|[\s]+|i"," ",$str);
+
+function pal($str) {
+    $str2 = '';
+    for ($i=strlen($str)-1; $i>=0; $i--){
+        $str2 .= $str[$i];
+    }
+    if ($str2 == $str){
+        echo "<h1> Word is palindrome </h1>";
+    } else {
+        echo "<h1> Not palindrome </h1>";
+    }
 }
-if ($str2 == $str){
-    echo "<h1> Word is palindrome </h1>";
-} else {
-    echo "Not palindrome";
+
+pal($str);
+
+// Что-то похожее на ООП :)
+class Pal {
+    function __construct($str) {
+        $str2 = '';
+        for ($i=strlen($str)-1; $i>=0; $i--){
+            $str2 .= $str[$i];
+        }
+        if ($str2 == $str){
+            echo "<h1> Word is palindrome </h1>";
+        } else {
+            echo "<h1> Not palindrome </h1>";
+        }
+    }
 }
+
+$palindrome = new Pal($str);
