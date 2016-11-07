e96b889e7fb0bde6cfd67ec646831e40167bc412
Ximalis <ximalis@yandex.ru>
2016-11-07 22:19:20 +0200

    Done

--- LongestWord.php
+++ LongestWord.php
@@ -2,18 +2,14 @@
 
 function findLongestWord($str)
 {
-    $str2 = '';
-    $curlen = 0;
-    $maxlen = 0;
-    $index = 0;
-    for ($i = 0; $i <= strlen($str) - 1; $i++) {
-        if ($str == ' ') {
-           /* $str = $curlen;
-            $index++;*/
-        }
+    $str = explode(" ", $str);
+    $res = '';
+    foreach($str as $v) {
+        if(strlen($res) < strlen($v))
+            $res = $v;
+
     }
-   /* $str = explode(" ", $str);
-    print_r($str) ;*/
+    echo 'Length of the longest word is ' .  strlen($res) . ' symbols';
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
