c7dffaf9a07fc280baab247ede6ce2b651823eba
Ximalis <ximalis@yandex.ru>
2016-11-07 21:31:07 +0200

    Create LongestWord.php

---
+++ LongestWord.php
@@ -0,0 +1,19 @@
+<?php
+
+function findLongestWord($str)
+{
+    $str2 = '';
+    $curlen = 0;
+    $maxlen = 0;
+    $index = 0;
+    for ($i = 0; $i <= strlen($str) - 1; $i++) {
+        if ($str == ' ') {
+           /* $str = $curlen;
+            $index++;*/
+        }
+    }
+   /* $str = explode(" ", $str);
+    print_r($str) ;*/
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
